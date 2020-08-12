70bcaa4f7d08a40d19b49d20a9f3798379e75e3a
Ka Yuen Leung <thomas060142@icloud.com>
2020-08-12 15:38:02 +0800

    Updates

---
+++ precache-manifest.5823189cdc98d377f10e9d14fbe86bea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64022611102bd431c85cf1cd2523eba",
+    "url": "/patrol_demo/index.html"
+  },
+  {
+    "revision": "ef30b1cb181158564179",
+    "url": "/patrol_demo/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6b6eca2f33f6f1c616be",
+    "url": "/patrol_demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef30b1cb181158564179",
+    "url": "/patrol_demo/static/js/2.a4790ce3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/patrol_demo/static/js/2.a4790ce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b6eca2f33f6f1c616be",
+    "url": "/patrol_demo/static/js/main.5d0f49c2.chunk.js"
+  },
+  {
+    "revision": "57088897ea8ad43bdc55",
+    "url": "/patrol_demo/static/js/runtime-main.ae883571.js"
+  }
+]);
